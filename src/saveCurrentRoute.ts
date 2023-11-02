215c09907ad5e40c562119a3142abb36e2c9718f
Oleh Kandaskalov <olegkandaskalov@gmail.com>
2023-11-03 00:50:51 +0200

    change moduleResolution

--- src/saveCurrentRoute.ts
+++
@@ -1,5 +0,0 @@
-import { RouteLocationNormalized } from 'vue-router';
-
-export function saveCurrentRoute(to: RouteLocationNormalized, from: RouteLocationNormalized): void {
-  localStorage.setItem('savedRoute', to.path);
-}
